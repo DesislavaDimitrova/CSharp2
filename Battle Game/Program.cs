bedc876ffdf1426008d245c7073088d322fe3273
DesislavaDimitrova <mascotte7373@gmail.com>
2024-06-28 19:35:18 +0300

    Add mini oop project

---
+++ Battle Game/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit playerOne = new Unit(10, 37, 40, "PlayerOne");
+            Unit playerTwo = new Unit(110, 20, 15, "PlayerTwo");
+
+            while(!playerOne.IsDead && !playerTwo.IsDead)
+                {
+                Console.WriteLine("First player's turn. Choose an action: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "attack")
+                {
+                    playerOne.Attack(playerTwo);
+                }
+                else
+                {
+                    playerOne.Heal();
+                }
+
+                if (playerOne.IsDead || playerTwo.IsDead)
+                    break;
+
+                Console.WriteLine($"Heatlh: {playerOne.UnitName} = {playerOne.Health} {playerTwo.UnitName} = {playerTwo.Health}");
+
+                Console.WriteLine("Second player's turn. Choose an action: ");
+                choice = Console.ReadLine();
+
+                if (choice == "attack")
+                {
+                    playerTwo.Attack(playerOne);
+                }
+                else
+                {
+                    playerOne.Heal();
+                }
+
+                Console.WriteLine($"Heatlh: {playerOne.UnitName} = {playerOne.Health} {playerTwo.UnitName} = {playerTwo.Health}");
+            }
+        }
+    }
+}
---
+++ Battle Game/Unit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Game
+{
+    internal class Unit
+    {
+        private int maxHealth;
+        private int currentHealth;
+        private int power;
+        private int heal;
+        private string unitName;
+        private Random random;
+
+        public int Health { get { return currentHealth; } }
+        public string UnitName { get { return unitName; } }
+
+        public bool IsDead {  get { return currentHealth <= 0; } }
+
+
+
+
+        public Unit(int maxHealth, int power, int heal, string unitName)
+        {
+            this.maxHealth = maxHealth;
+            this.currentHealth = maxHealth;
+            this.power = power;
+            this.heal = heal;
+            this.unitName = unitName;
+            this.random = new Random();
+            
+        }
+
+        public void Attack(Unit unitToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int rndDamage = (int)(power * rng);
+            unitToAttack.TakeDamage(rndDamage);
+            Console.WriteLine($"{UnitName} took {rndDamage} health points from {unitToAttack.unitName}");
+        }
+
+        void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            if (IsDead)
+            {
+                Console.WriteLine($"{UnitName} is dead!");
+            }
+        }
+
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int healPoints = (int)(heal * rng);
+            currentHealth = currentHealth + healPoints > maxHealth ? maxHealth: healPoints;
+        }
+
+
+
+    }
+}
